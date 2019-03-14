20882acb510aa35bc296622b2ae6f418e2ee9adc
brianmodel <brian.model@gmail.com>
2019-03-14 19:41:13 -0400

    started working on model

--- Orchestration/__init__.py
+++ Orchestration/__init__.py
@@ -1,4 +1,6 @@
 from os.path import dirname, abspath, join
+import torch
 
 base_path = dirname(dirname(abspath(__file__)))
-data_path = join(dirname(dirname(abspath(__file__))), 'data')
+data_path = join(dirname(dirname(abspath(__file__))), "data")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
